63fabcca52cbedafdfba1b0ee1de50f5652c4099
harshrishimiglani <miglaniharshrishi@gmail.com>
2022-02-14 00:17:12 +0530

    Assignment 3

---
+++ asg3_outputFile_CSE_21103095.py
@@ -0,0 +1,88 @@
+QUESTION 1
+ENTER ANY STRING: 
+HARSH
+{'H': 2, 'A': 1, 'R': 1, 'S': 1}
+
+
+QUESTION 2
+ENTER THE DAY: 
+13
+ENTER THE MONTH OF THE YEAR: 
+03
+ENTER THE YEAR: 
+2002
+14 / 3 / 2002
+
+
+QUESTION 3
+Enter elements of a list separated by space 
+4 8 9
+list:  4 8 9
+[(4, 16), (8, 64), (9, 81)]
+
+
+QUESTION 4
+Enter Grade Point: 
+7
+Your Grade is 'B' and Good performance
+
+
+QUESTION 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+QUESTION 6
+Enter SID: 
+21103095
+Enter Name: 
+HARSH
+Do you want to enter another student entry(Y or N):
+Y
+Enter SID: 
+21103094
+Enter Name: 
+JOGI
+Do you want to enter another student entry(Y or N):
+Y
+Enter SID: 
+21103093
+Enter Name: 
+GAURAV
+Do you want to enter another student entry(Y or N):
+N
+<a> {21103095: 'HARSH', 21103094: 'JOGI', 21103093: 'GAURAV'}
+<b> {21103093: 'GAURAV', 21103095: 'HARSH', 21103094: 'JOGI'}
+<c> {21103093: 'GAURAV', 21103094: 'JOGI', 21103095: 'HARSH'}
+Search Name with SID: 
+21103094
+<d> JOGI
+QUESTION 7
+ENTER THE NUMBER OF TERMS IN THE SERIES: 
+10
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 8.8
+QUESTION 8
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
