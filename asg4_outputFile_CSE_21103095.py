7d3f5c7a3c14d35deac20b1c1f05b040de04470a
harshrishimiglani <miglaniharshrishi@gmail.com>
2022-02-26 21:50:11 +0530

    Assignment 4

---
+++ asg4_outputFile_CSE_21103095.py
@@ -0,0 +1,58 @@
+ANSWER 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+ANSWER 2
+Enter the size of pascals triangle 6
+USING LOOPS
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+USING RECURSSIONS
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+
+ANSWER 3
+Enter 2 numbers: 9 4
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> All the values are non zero
+<c> Filtered out numbers that are greater than 4 are : [6, 5, 6, 7, 6, 8, 7]
+<d> Set: {8, 5, 6, 7}
+<e> Immutable set: frozenset({8, 5, 6, 7})
+<f> Hash value of the max value from the above set: 8
+
+
+ANSWER 4 
+Object is created
+The name of the student it Harsh Rishi and SID is 21103095.
+Object destroyed
+
+
+ANSWER 5
+(a) The updated salary of Mehak is 70000 
+(b)Record of Viren deleted
+
+ANSWER 6
+Enter the word: lead
+Enter a new meaningful word using the exact same letters to test your friendship: deal
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
